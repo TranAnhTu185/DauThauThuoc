ab791d400b7d8eae27a7033c7ce6446384541214
trananhtu8900@gmail.com <trananhtu8900@gmail.com>
2022-05-16 17:57:38 +0700

    tttt

--- vs code/vs code/asset/js/main.js
+++ vs code/vs code/asset/js/main.js
@@ -1,3 +1,11 @@
+const MenuMoble = document.querySelector('.menu__mobile');
+const NavMenuNobule = document.querySelector('.nav__menu-mobile');
+const CloseMenu = document.querySelector('.close-menu');
+let animateMuis =  document.querySelectorAll('.animate-mui');
+let procedureContainers = document.querySelectorAll('.procedure-container');
+let ProcedureDes = document.querySelectorAll('.procedure-description');
+
+console.log(ProcedureDes);
 window.onscroll = () => {
     if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
       document.querySelector('.header').classList.add('nav');
@@ -7,16 +15,44 @@ window.onscroll = () => {
     }
 }
 
-document.querySelector('.menu__mobile').onclick = () => {
-    document.querySelector('.nav__menu-mobile').classList.add('show');
-    document.querySelector('.menu__mobile').style.display = "none";
-    document.querySelector('.close-menu').style.width = "16%";
+MenuMoble.onclick = () => {
+  NavMenuNobule.classList.add('show');
+  MenuMoble.style.display = "none";
+  CloseMenu.style.width = "16%";
 }
 
-document.querySelector('.close-menu').onclick = () => {
-    
-    document.querySelector('.nav__menu-mobile.show').classList.remove('show');
-    // document.querySelector('.nav__menu-mobile.show').style.animation = "fadeOutMenubobile 2s linear";
-    document.querySelector('.menu__mobile').style.display = "flex";
-    document.querySelector('.close-menu').style.width = "0";
+CloseMenu.onclick = () => {
+  document.querySelector('.nav__menu-mobile.show').classList.remove('show');
+  MenuMoble.style.display = "flex";
+  CloseMenu.style.width = "0";
 }
+
+let indexActive = 0;
+
+setInterval(() => {
+  animateMuis.forEach(item => {
+    if (item.classList.contains('active')) {
+        item.classList.remove('active');
+    }
+  }) 
+
+  ProcedureDes.forEach(pro => {
+    if(indexActive + 1 > 8 ) {
+      ProcedureDes[0].classList.add('active');
+    }
+    if(pro.classList.contains('active') && indexActive + 1 <= 8) {
+      pro.classList.remove('active');
+    }
+  })
+
+
+  if (indexActive == animateMuis.length) {
+    ProcedureDes[0].classList.add('active');
+    setTimeout(() => {
+      indexActive = 0;
+    },1000)
+  }
+  animateMuis[indexActive].classList.add('active');
+  ProcedureDes[indexActive + 1].classList.add('active');
+  indexActive++;
+}, 2000)
